047002a74d3389811372fa90a1ce7d61c1874da6
dan cooper <88119677+dancoopper@users.noreply.github.com>
2021-08-26 15:44:24 -0400

    Add files via upload

---
+++ FR bot.py
@@ -0,0 +1,34 @@
+import pyautogui
+from time import sleep
+
+
+print('''
+ _   _             _          _  ______    _                _  ______                           _   
+| | | |           (_)        | | |  ___|  (_)              | | | ___ \                         | |  
+| |_| |_   _ _ __  ___  _____| | | |_ _ __ _  ___ _ __   __| | | |_/ /___  __ _ _   _  ___  ___| |_ 
+|  _  | | | | '_ \| \ \/ / _ \ | |  _| '__| |/ _ \ '_ \ / _` | |    // _ \/ _` | | | |/ _ \/ __| __|
+| | | | |_| | |_) | |>  <  __/ | | | | |  | |  __/ | | | (_| | | |\ \  __/ (_| | |_| |  __/\__ \ |_ 
+\_| |_/\__, | .__/|_/_/\_\___|_| \_| |_|  |_|\___|_| |_|\__,_| \_| \_\___|\__, |\__,_|\___||___/\__|
+        __/ | |                                                              | |                    
+       |___/|_|                                                              |_|                    
+''')
+print("this is the first version")
+ign = input("what is the ign: ")
+
+sleep(4)
+
+
+while True:
+        pyautogui.press('enter')
+        pyautogui.typewrite("/f " + ign)
+        pyautogui.press('enter')
+        print("Friend request sent to " + ign)
+        sleep(60)
+        print("one min down")
+        sleep(60)
+        print("two min down")
+        sleep(60)
+        print("thee min down")
+        sleep(60)
+        print("4 min down")
+        sleep(60)
