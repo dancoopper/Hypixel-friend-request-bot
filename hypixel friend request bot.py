0e66bb24049d8e7ef6ab09b9aa791e2f491cc643
dan cooper <88119677+dancoopper@users.noreply.github.com>
2021-08-26 15:40:43 -0400

    Update hypixel friend request bot.py

--- hypixel friend request bot.py
+++ hypixel friend request bot.py
@@ -2,25 +2,18 @@ import pyautogui
 from time import sleep
 
 
+print('''
+ _   _             _          _  ______    _                _  ______                           _   
+| | | |           (_)        | | |  ___|  (_)              | | | ___ \                         | |  
+| |_| |_   _ _ __  ___  _____| | | |_ _ __ _  ___ _ __   __| | | |_/ /___  __ _ _   _  ___  ___| |_ 
+|  _  | | | | '_ \| \ \/ / _ \ | |  _| '__| |/ _ \ '_ \ / _` | |    // _ \/ _` | | | |/ _ \/ __| __|
+| | | | |_| | |_) | |>  <  __/ | | | | |  | |  __/ | | | (_| | | |\ \  __/ (_| | |_| |  __/\__ \ |_ 
+\_| |_/\__, | .__/|_/_/\_\___|_| \_| |_|  |_|\___|_| |_|\__,_| \_| \_\___|\__, |\__,_|\___||___/\__|
+        __/ | |                                                              | |                    
+       |___/|_|                                                              |_|                    
+''')
+print("this is the first version")
 ign = input("what is the ign: ")
-
-conferm = input(ign + " is that right? ")
-
-anser = ["yes", "Yes", "YES"]
-
-if conferm == anser:
-    print(5)
-    sleep(1)
-    print(4)
-    sleep(1)
-    print(3)
-    sleep(1)
-    print(2)
-    sleep(1)
-    print(1)
-    pass
-else:
-    ign = input("what is the right ign: ")
 
 sleep(4)
 
@@ -30,12 +23,12 @@ while True:
         pyautogui.typewrite("/f " + ign)
         pyautogui.press('enter')
         print("Friend request sent to " + ign)
-        sleep(4)
+        sleep(60)
         print("one min down")
-        sleep(4)
+        sleep(60)
         print("two min down")
-        sleep(4)
+        sleep(60)
         print("thee min down")
-        sleep(4)
+        sleep(60)
         print("4 min down")
-        sleep(4)
+        sleep(60)
